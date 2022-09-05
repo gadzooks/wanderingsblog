7ea71dc9512c0a25d53fc48b27c3f3f12aac76ec
gadzooks <gadzooks@users.noreply.github.com>
2022-09-05 21:41:15 +0000

    jekyll build from Action 8aa745cf9c215d35b77813109e335a48e102c4b1

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,114 @@
+
+var documents = [{
+    "id": 0,
+    "url": "/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "/about/",
+    "title": "About",
+    "body": "This is my attempt to document some of the most amazing places that I have been to, the people I have met and things I learnt. "
+    }, {
+    "id": 2,
+    "url": "/about",
+    "title": "Mediumish Template for Jekyll",
+    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    }, {
+    "id": 3,
+    "url": "/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 4,
+    "url": "/",
+    "title": "Home",
+    "body": "      Featured:                                                                                                                                                                                                           Costa Rica - part deux                              :               I fell in love with Costa Rica the first time I went there. I was my first trip outside of the US and I was. . . :                                                                                                                                                                       Amit                                10 Apr 2022                                                                                                                                                                                                                                                                                                                  My first solo backpacking with Baloo                              :               This was my first “solo” backpacking adventure. I decided to go on a long 4 day hike in the North Cascades and I picked a. . . :                                                                                                                                                                       Amit                                27 Aug 2017                                                                                                                            All Stories:                                                                                                     Costa Rica - part deux              :       I fell in love with Costa Rica the first time I went there. I was my first trip outside of the US and I was amazed by the natural beauty. . . :                                                                               Amit                10 Apr 2022                                                                                                                                     My first solo backpacking with Baloo              :       This was my first “solo” backpacking adventure. I decided to go on a long 4 day hike in the North Cascades and I picked a tough scenic one - North. . . :                                                                               Amit                27 Aug 2017                                                                                                                                     Mt Dickerman Winter Route              :        Mt Dickerman is one of the premier hikes in the Pacific North West. It is a beast of a climb but the rewards are amazing. :                                                                               Amit                05 Jun 2017                                            "
+    }, {
+    "id": 5,
+    "url": "/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 6,
+    "url": "/costa-rica-trip/",
+    "title": "Costa Rica - part deux",
+    "body": "2022/04/10 -  I fell in love with Costa Rica the first time I went there. I was my first trip outside of the US and I was amazed by the natural beauty and kindness of the people.  Our itinerary / high level plan Local services we used What you will need to bring with you Driving     San Jose to Arenal :   Arenal to Monte Verde :   Monte Verde to Jaco   Jaco to San Jose   Our itinerary / high level plan      Date   Activity   Driving   Stay   Notes         1   Fly into San Jose   -   Melrost Airport B&amp;B           2   Drive to Arenal   3-4 hrs   Arenal - Volcano Lodge, Hotel &amp; Thermal Experience   Pick up car Vamos was great       3   Canoa Negro Float    Hotel Pickup   Arenal   One of the best ways to see wild life       4   Hanging Bridge, Arenal 1968   None   Arenal   1968 trail was amazing       5   Drive to Monte Verde   3-4 hours   Hotel Claro de Luna   best roads are Route 142 to Tilaran, then Route 145 to 606. This drive takes about 3. 5-4 hours (from La Fortuna) and is along a mix of paved and dirt roads       6   In Monte Verde   None   Monte Verde   https://reservacuricancha. com/ You can also give us a call at: (506) 2645-6915 / (506) 8448-8283 Starts at 6:00.        7   Drive to Jaco       Hotel Club del Mar Oceanfront   Hotel was nice. Beach town for locals.        8   Drive to San Jose       Marriott Hotel Hacienda Belen   Beautiful hotel. Great dinner and some tennis in sandals       9   Fly to Seattle       Marriott Hotel       Local services we used Car rental (Vamos). They dropped off and picked up the car from the hotels we stayed in for a very reasonable price ($20ish) Get a SIM card with Kolbi. great coverageWhat you will need to bring with you Head lamps Baby wipes ? Small hand sanitizer(s) Diarreah pills Arnica cream or gel - in case of bumps, bruises, etc - it works wonders.  Ponchos / small umbrella Money belt Sony camera + extra batteries + charger Water shoes for beach and ocean Spanish language book / dictionary / translation app Plastic bags, zip log bags Several color copies of passport Long socks , up to knees TP roll ? Take cash $400 or so each Chafing cream Bandana Small first aid kit Summit packs (2?) Travel insurance for United flightDriving Driving in CR is very peaceful. The traffic is well behaved, especially once you are out of the capital San Jose. We planned our rentals in such a way that Vamos delivered the car to us on our 1st day of the rental and picked it up on the last day from a different location. This is the way to do it instead of figuring out a way to get to their office in SJ on pickup and return. San Jose to Arenal :: San Jose to Arenal: If you are coming from San Jose, it should take you no more than 3-4 hours to get to La Fortuna, Costa Rica. Traveling west on Route 1 will take you to San Ramon where you will then take Route 702 to La Fortuna. The total driving distance from La Fortuna/Arenal to San Jose is 139 km (86 miles). Although it doesn’t sound like a long distance, the winding roads will slow you down so be sure to plan accordingly. Arenal to Monte Verde :: ￼ Monte Verde to Jaco: ￼ Jaco to San Jose: ￼ "
+    }, {
+    "id": 7,
+    "url": "/1st-backpacking-with-baloo/",
+    "title": "My first solo backpacking with Baloo",
+    "body": "2017/08/27 -  This was my first “solo” backpacking adventure. I decided to go on a long 4 day hike in the North Cascades and I picked a tough scenic one - North Fork Sauk Trail Some of the things that made this very challenging were :  My first solo trip It was about 30 miles, in high country and it was a hot summer week.  I had no idea that Baloo gets so hot and needs a lot of water himself.   Baloo resting after another long day of hiking in the hot sun&copy; Flickr/Am-it All Rights Reserved  Mountains bathing in alipine glow!&copy; Flickr/Am-it All Rights Reserved  Looking out into the valley&copy; Flickr/Am-it All Rights Reserved  Beautiful sunset&copy; Flickr/Am-it All Rights Reserved  Bear grass&copy; Flickr/Am-it All Rights Reserved"
+    }, {
+    "id": 8,
+    "url": "/mt-dickerman-winter-route/",
+    "title": "Mt Dickerman Winter Route",
+    "body": "2017/06/05 -  Mt Dickerman is one of the premier hikes in the Pacific North West. It is a beast of a climb but the rewards are amazing. I decided to climb via the winter route for a change. The winter route, mainly follows the summer route but towards the top instead of taking switch-backs you go straight up as the crow flies. This hike is not to be taken lightly, even in the best of situations and especially not in winter conditions.   Baloo saying - lets go hike already !&copy; Flickr/Am-it All Rights ReservedNot too shabby along the way too:   Blue bird day&copy; Flickr/Am-it All Rights Reserved  My buddy Eric hiking up&copy; Flickr/Am-it All Rights Reserved  Final Ascent&copy; Flickr/Am-it All Rights Reserved  Beautiful but deadly cornices&copy; Flickr/Am-it All Rights Reserved  Crowded at the top ?&copy; Flickr/Am-it All Rights ReservedAt the very top:   Mountains as far as you can see&copy; Flickr/Am-it All Rights Reserved  Winter makes everything extra magical&copy; Flickr/Am-it All Rights Reserved  Doggie paradise&copy; Flickr/Am-it All Rights Reserved  Snow capped mountains&copy; Flickr/Am-it All Rights Reserved"
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
