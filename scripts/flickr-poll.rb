c58355f769b05dacbe1d3e786a59038ca31d243c
gadzooks <gadzooks@users.noreply.github.com>
2023-03-24 04:43:17 +0000

    deploy: d55bfd722b742f8b2f4787e5ea1ad40f6950d8c2

--- scripts/flickr-poll.rb
+++ scripts/flickr-poll.rb
@@ -2,6 +2,7 @@
 require 'flickr'
 require "down"
 require "fileutils"
+require "openai"
 
 # The credentials can be provided as parameters:
 
@@ -12,11 +13,7 @@ require "fileutils"
 # ENV['FLICKR_API_KEY']
 # ENV['FLICKR_SHARED_SECRET']
 
-PostDetails = Struct.new(:featured, :photoset, :main_photo, :post_file_name, keyword_init: true) do 
-  def categories
-    main_photo.tags
-  end
-
+PostDetails = Struct.new(:featured, :photoset, :main_photo, :description, keyword_init: true) do 
   def image_alt_text
     photoset.title
   end
@@ -25,9 +22,41 @@ PostDetails = Struct.new(:featured, :photoset, :main_photo, :post_file_name, key
     dir_path = './assets/images/' +  main_photo["datetaken"].split(' ').first + '/'
   end
 
-  def image_file_name
-    main_photo['title'].gsub(' ', '-') + '.jpg'
+  def post_id
+    str = main_photo['title'].strip.empty? ? categories : main_photo['title']
+    str.gsub(/\s+/, ' ').gsub(' ', '-')
   end
+
+  def image_file_name
+     self.post_id + '.jpg'
+  end
+
+  def categories
+    main_photo.tags.split(' ').select {|tag| !['jsu', 'js', 'jsd', 'main'].include?(tag)}.uniq.join(' ')
+  end
+
+  def post_file_name
+    file_name = main_photo["datetaken"].split(' ').first + '-' + self.post_id
+    file_path = '_posts/' + file_name + '.markdown'
+    return file_path
+  end
+
+  def save_main_image
+    # dir_path = './assets/images/' +  main_photo["datetaken"].split(' ').first + '/'
+    FileUtils.mkdir_p(self.image_dir)
+    file_name = self.image_dir + self.image_file_name
+    if File.exists? file_name
+      puts "Image already downloaded. Skipping : " + file_name
+      return file_name
+    end
+    url = main_photo['url_m']
+    tempfile = Down.download(url)
+    FileUtils.mv(tempfile.path, "#{file_name}")
+
+    return file_name
+  end
+
+
 end
 
 class Main
@@ -35,25 +64,6 @@ class Main
 
     Flickr.cache = '/tmp/flickr-api.yml'
     flickr = Flickr.new
-
-    # Flickr will raise an error if either parameter is not explicitly provided, or available via environment variables.
-
-    list   = flickr.photos.getRecent
-
-    id     = list[0].id
-    secret = list[0].secret
-    info   = flickr.photos.getInfo :photo_id => id, :secret => secret
-
-    puts info.title           # => "PICT986"
-    puts info.dates.taken     # => "2006-07-06 15:16:18"
-
-    sizes = flickr.photos.getSizes :photo_id => id
-
-    original = sizes.find { |s| s.label == 'Original' }
-    # puts original.width       # => "800" -- may fail if they have no original marked image
-
-    info = flickr.photos.getInfo(:photo_id => "3839885270")
-    Flickr.url_short(info) # => "https://flic.kr/p/6Rjq7s"
 
     photos = flickr.people.getPublicPhotos(:user_id => '57125599@N00', :extras => 'description,tags,geo,date_taken,url_m', per_page: 25)
 
@@ -77,69 +87,38 @@ Not too shabby along the way too
 =end
 
     post_details_by_id = {}
+    photos_by_album_id = Hash.new {|h, k| h[k] = []} 
 
     photos.each do |photo|
-      puts photo.id
-      unless photo.tags.include?('jekyllsite')
-        next
-        puts "Skipping photo due to missing tag " + photo.tags + " " + photo.id
-      end
-      if photo.tags.include? 'main'
+      # FIXME : using the 1st photoset for now
+      context = flickr.photos.getAllContexts(photo_id: photo.id)['set'].last
+      if photo.tags.include?('js') && photo.tags.include?('main')
         puts "main photo found " + photo.inspect
-        post_file_name = get_post_filename(photo)
-        puts post_file_name
-        puts File.exists? '_posts/' + post_file_name
-        # FIXME : using the 1st photoset for now
-        context = flickr.photos.getAllContexts(photo_id: photo.id)['set'].last
-        # puts "photoset id : " + context.inspect
-        if post_details_by_id.include? context.id
+        if post_details_by_id.include?(context.id) && !photo.tags.include?('jsu')
           puts "already handled this photoset so skipping " + context.id 
         else
           puts "found new photoset " + context.id
-          post_details = PostDetails.new(featured: true, photoset: context, main_photo: photo, post_file_name: post_file_name)
+          # puts context.inspect
+          post_details = PostDetails.new(featured: photo.tags.include?('feature'), photoset: context, main_photo: photo, description: "")
+          post_details.description = chatgpt(post_details.categories)
           post_details_by_id[context.id] = post_details
-          # hsh['categories'] = photo.tags
-          # hsh['image_alt_text'] = context.title
         end
+      else
+        photos_by_album_id[context.id] << photo
       end
     end
 
-    post_details_by_id.values
+    { post_details_by_id: post_details_by_id, other_photos_by_album_id: photos_by_album_id }
   end
 
-  def self.save_main_image(photo)
-    dir_path = './assets/images/' +  photo["datetaken"].split(' ').first + '/'
-    FileUtils.mkdir_p(dir_path)
-    file_name = dir_path + photo['title'].gsub(' ', '-') + '.jpg'
-    if File.exists? file_name
-      puts "Image already downloaded. Skipping : " + file_name
-      return file_name
-    end
-    url = photo['url_m']
-    tempfile = Down.download(url)
-    FileUtils.mv(tempfile.path, "#{file_name}")
+  FLICKR_IMAGE_TEMPLATE = '
+  flickr %{photo_id} "%{photo_title}" style="float: right;"
+  '
 
-    return file_name
-  end
-
-  def self.get_post_filename(photo)
-    # {"id"=>"52762914260", "owner"=>"57125599@N00", "secret"=>"14aa2ef94b", "server"=>"65535", "farm"=>66, "title"=>"Main",
-    # "ispublic"=>1, "isfriend"=>0, "isfamily"=>0, "description"=>"Great hike", "datetaken"=>"2023-03-16 12:25:05",
-    # "datetakengranularity"=>0, "datetakenunknown"=>"0", "tags"=>"jekyllsite anothertag yet another tag main", 
-    # "latitude"=>"47.429444", "longitude"=>"-121.381578", "accuracy"=>"16", "context"=>0, "place_id"=>"", "woeid"=>"5798083", 
-    # "geo_is_public"=>1, "geo_is_contact"=>0, "geo_is_friend"=>0, "geo_is_family"=>0, 
-    # "url_m"=>"https://live.staticflickr.com/65535/52762914260_14aa2ef94b.jpg", "height_m"=>500, "width_m"=>361}
-
-    file_name = photo["datetaken"].split(' ').first + '-' + photo["title"].gsub(' ', '-')
-    file_path = '_posts/' + file_name + '.markdown'
-    puts file_path
-    puts File.exists? file_path
-    return file_path
-  end
-
-  # result = "Breed %{b} size %{z}" % {b: breed, z: size}
   POST_TEMPLATE = '---
 layout: post
+title: %{title}
+date: %{date}
 categories: %{categories}
 author: amit
 image: %{image_path}
@@ -148,19 +127,53 @@ featured: %{featured}
 photoset: %{photoset_id}
 ---
 %{description}
+
+%{flickr_images}
   '
 
-  def self.create_post(post_details)
-    puts post_details.inspect
-    file_path = post_details[:post_file_name]
-    puts "post file path : #{file_path}"
+  def self.create_post(post_details, other_photos_by_album, overwrite = true)
+    post_hash = {
+      post_file_name: post_details.post_file_name,
+      title: post_details.photoset['title'],
+      date: post_details.main_photo['datetaken'],
+      categories: post_details.categories,
+      image_path: post_details.save_main_image,
+      image_alt_text: post_details.photoset['title'],
+      featured: post_details.featured,
+      photoset_id: post_details.photoset['id'],
+      description: post_details.description
+    }
+
+    file_path = post_details.post_file_name
+
     if File.exists? file_path
-      puts file_path + ' already exists. NOT overriding'
-      return
+      if overwrite
+        puts 'overwrite flag is set so deleting existing file'
+        File.delete(file_path)
+      else
+        puts file_path + ' already exists. NOT overriding'
+        return
+      end
     end
 
-    post_str = POST_TEMPLATE % post_details
-    puts "writing to file : " + file_path
+    # TODO : flickr tag plugin not working so commented out
+    flickr_images = ''
+    # other_photos = other_photos_by_album[post_details.photoset['id']]
+    # other_photos.each_with_index do |photo, i|
+    #   puts photo.inspect
+    #   hsh = {
+    #     photo_id: photo['id'],
+    #     photo_title: (photo['title'] || ''),
+    #   }
+    #   str = FLICKR_IMAGE_TEMPLATE % hsh
+    #   flickr_images += "{% #{str} %}"
+    #   break if i == 5
+    # end
+
+    post_hash[:flickr_images] = flickr_images
+
+    post_str = POST_TEMPLATE % post_hash
+    # puts "writing to file : " + file_path
     File.open(file_path, 'w') do |out_file|
       out_file.puts post_str
     end
@@ -168,43 +181,63 @@ photoset: %{photoset_id}
   end
 
   def self.run
-    all_post_details = get_flickr_updates()
-    all_post_details.each do |post_details|
-      puts '-------------------'
-      puts post_details.inspect
-      image_path = save_main_image(post_details.main_photo)
-      post_hash = {
-        post_file_name: post_details.post_file_name,
-        categories: post_details.categories,
-        image_path: image_path,
-        image_alt_text: post_details.photoset['title'],
-        featured: post_details.featured,
-        photoset_id: post_details.photoset['id'],
-        description: post_details.main_photo['description']
-      }
-      create_post(post_hash)
+    data = get_flickr_updates()
+
+
+    data[:post_details_by_id].each do |photoset_id, post_details|
+      create_post(post_details, data[:other_photos_by_album_id])
     end
+  end
+
+  def self.chatgpt(description)
+    OpenAI.configure do |config|
+      config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+    end
+
+    client = OpenAI::Client.new
+
+    prompt = "Write 2 paragraphs on middle fork trail hiking snow winter river in wa state"
+    prompt = "write 2 paragraphs on #{description}"
+    prompt = "write description with keywords #{description}"
+    puts '-----------------'
+    puts prompt
+
+    # response = client.chat(
+    #   parameters: {
+    #     model: "text-davinci-003",
+    #     prompt: prompt,
+    #     # temperature: 0, # show the low risk text options
+    #     max_tokens: 256,
+    #     # frequency_penalty: 0,
+    #     # presence_penalty: 0,
+    # })
+    # puts response["choices"].map { |c| c["text"] }
+
+    response = client.completions(
+      parameters: {
+          model: "text-davinci-003",
+          prompt: prompt,
+          max_tokens: 256
+      })
+  puts response["choices"].map { |c| c["text"] }
+
+  puts response.inspect
+  # => [", there lived a great"]
+
+    # response = client.chat(
+    #   parameters: {
+    #       model: "gpt-3.5-turbo", # Required.
+    #       messages: [{ role: "user", content: "Hello!"}], # Required.
+    #       temperature: 0.7,
+    #   })
+    # puts response.dig("choices", 0, "message", "content")
+
+    # => "Hello! How may I assist you today?"
+
+    return response["choices"].first["text"] 
+
   end
 
 end
 
 Main.run
-
-# file_name = Main.get_post_filename({'datetaken' => '2023-03-16 12:25:05', 'title' => 'mt dickerman winter route 2023'})
-
-# post_details = {categories: 'snow capped mt', image_path: 'assets/images/06-05-17/mt-dickerman.jpg',
-#   alt_image_text: 'mt dickerman', photoset_id: 72177720306901699, description: 'some desci'
-# }
-# Main.create_post(file_name, post_details)
-
-# photo = {"id"=>"52762914260", "owner"=>"57125599@N00", "secret"=>"14aa2ef94b", "server"=>"65535", "farm"=>66, "title"=>"Main", "ispublic"=>1, "isfriend"=>0, "isfamily"=>0, "description"=>"Great hike", "datetaken"=>"2023-03-16 12:25:05", "datetakengranularity"=>0, "datetakenunknown"=>"0", "tags"=>"jekyllsite anothertag yet another tag main", "latitude"=>"47.429444", "longitude"=>"-121.381578", "accuracy"=>"16", "context"=>0, "place_id"=>"", "woeid"=>"5798083", "geo_is_public"=>1, "geo_is_contact"=>0, "geo_is_friend"=>0, "geo_is_family"=>0, "url_m"=>"https://live.staticflickr.com/65535/52762914260_14aa2ef94b.jpg", "height_m"=>500, "width_m"=>361}
-# Main.save_main_image(photo)
-
-# get all photos in last 1h
-# get unique albums for each photo with a main photo
-# for each album see if there is a post already created - store in yml file ? 
-# if not found 
-  #  get album meta data to make draft post
-  #  create new draft post via jekyll draft command line
-  #  add tag listing that photo was added to jekyll blog
-  #  print out message
