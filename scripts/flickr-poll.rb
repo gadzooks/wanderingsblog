9be2bd0a54fa248f9267479119caa4f9c20844e1
gadzooks <gadzooks@users.noreply.github.com>
2023-05-13 13:53:17 +0000

    deploy: a3960a0877fb2103bd9ca8bfee457366a26df406

--- scripts/flickr-poll.rb
+++ scripts/flickr-poll.rb
@@ -3,6 +3,8 @@ require 'flickr'
 require "down"
 require "fileutils"
 require "openai"
+require "date"
+require "set"
 
 # The credentials can be provided as parameters:
 
@@ -13,9 +15,13 @@ require "openai"
 # ENV['FLICKR_API_KEY']
 # ENV['FLICKR_SHARED_SECRET']
 
-PostDetails = Struct.new(:featured, :photoset, :main_photo, :description, keyword_init: true) do 
+PostDetails = Struct.new(:featured, :photoset, :main_photo, :categories, :description, keyword_init: true) do 
   def image_alt_text
     photoset.title
+  end
+
+  def date_taken
+    @date_taken ||= main_photo["datetaken"].split(' ').first
   end
 
   def image_dir
@@ -23,16 +29,20 @@ PostDetails = Struct.new(:featured, :photoset, :main_photo, :description, keywor
   end
 
   def post_id
-    str = main_photo['title'].strip.empty? ? categories : main_photo['title']
-    str.gsub(/\s+/, ' ').gsub(' ', '-')
+    @post_id ||= if main_photo['title'].strip.empty? 
+                    main_photo['title'].gsub(/\s+/, ' ').gsub(' ', '-')
+                    content = "pick one name of a place from #{categories}"
+                    messages = Main.compute_turbo_input(content)
+                    res = Main.chatgpt_turbo_35(messages)
+                    res.downcase.gsub(' ', '-')
+                  else 
+                    main_photo['title'].gsub(/\s+/, ' ').gsub(' ', '-')
+                  end
+    @post_id
   end
 
   def image_file_name
      self.post_id + '.jpg'
-  end
-
-  def categories
-    main_photo.tags.split(' ').select {|tag| !['jsu', 'js', 'jsd', 'main'].include?(tag)}.uniq.join(' ')
   end
 
   def post_file_name
@@ -43,13 +53,20 @@ PostDetails = Struct.new(:featured, :photoset, :main_photo, :description, keywor
 
 end
 
+PHOTOSETS_ADD_ENTRIES = '72177720307946395'
+USER_ID = '57125599@N00'
+PUBLIC_PHOTOS = 1
+META_DATA = 'description,tags,date_taken,url_m,widths,sizes,views'
+
 class Main
   def self.get_flickr_updates
 
     Flickr.cache = '/tmp/flickr-api.yml'
     flickr = Flickr.new
 
-    photos = flickr.people.getPublicPhotos(:user_id => '57125599@N00', :extras => 'description,tags,geo,date_taken,url_m,widths,sizes', per_page: 25)
+    # photos = flickr.people.getPublicPhotos(:user_id => '57125599@N00', :extras => 'description,tags,geo,date_taken,url_m,widths,sizes', per_page: 25)
+    photos = flickr.photosets.getPhotos(user_id: USER_ID, photoset_id: PHOTOSETS_ADD_ENTRIES, extras: META_DATA, privacy_filter: PUBLIC_PHOTOS)['photo'] || []
+    puts photos.inspect
 
 =begin
 ---
@@ -76,25 +93,57 @@ Not too shabby along the way too
     photos.each do |photo|
       contexts = flickr.photos.getAllContexts(photo_id: photo.id)['set']
       next unless contexts && !contexts.empty?
-      # FIXME : using the 1st photoset for now
-      context = contexts.first
-      if photo.tags.include?('js') && photo.tags.include?('main')
-        puts "main photo found " + photo.inspect
-        if post_details_by_id.include?(context.id) && !photo.tags.include?('jsu')
-          puts "already handled this photoset so skipping " + context.id 
-        else
-          puts "found new photoset " + context.id
-          # puts context.inspect
-          post_details = PostDetails.new(featured: photo.tags.include?('feature'), photoset: context, main_photo: photo, description: "")
-          post_details.description = post_description(post_details, photo)
-          post_details_by_id[context.id] = post_details
-        end
-      else
-        photos_by_album_id[context.id] << photo
+      puts "--------------- contexts are ---------------- "
+      puts contexts.inspect
+      context = contexts.find {|c| c.id != PHOTOSETS_ADD_ENTRIES}
+      unless context
+        puts "for photo #{photo.id} could not find any other albums so skipping entry"
+        next
       end
+
+      raw_categories = photo.tags.split(' ').select {|tag| !['jsu', 'js', 'jsd', 'main'].include?(tag)}.uniq.join(' ')
+      if raw_categories.strip == ""
+        puts "skipping entry for photo #{photo.id} because no categories were found"
+        next
+      end
+
+      content = "Give me the valid words from #{raw_categories}"
+      messages = compute_turbo_input(content)
+      categories = chatgpt_turbo_35(messages)
+      post_details = PostDetails.new(featured: photo.tags.include?('feature'), photoset: context, main_photo: photo, categories: categories, description: "")
+      post_details.description = post_description(post_details, photo)
+      post_details_by_id[context.id] = post_details
+
+      # get 5 interesting pictures from that context (photoset) and add it to that post
+      photos_by_album_id[context.id] = get_interesting_photos_from_context(flickr, photo, context.id)
     end
 
     { post_details_by_id: post_details_by_id, other_photos_by_album_id: photos_by_album_id }
+  end
+
+  def self.compute_turbo_input(content)
+    return [
+        {
+            "role": "system",
+            "content": "You return valid words in groups as csv"
+        },
+        {
+            "role": "user",
+            "content": content
+        }
+    ]
+  end
+
+  def self.get_interesting_photos_from_context(flickr, photo, context_id)
+    date_taken = DateTime.parse(photo.datetaken).strftime('%Y-%m-%d')
+    puts "looking up all photos in album #{context_id}"
+    photos = (flickr.photosets.getPhotos(user_id: USER_ID, photoset_id: context_id, extras: META_DATA, privacy_filter: PUBLIC_PHOTOS)['photo'] || [])
+    
+    photos.sort! do |a, b|
+      b.views.to_i <=> a.views.to_i
+    end
+    
+    return photos[0..4]
   end
 
   def self.post_description(post_details, photo)
@@ -104,7 +153,13 @@ Not too shabby along the way too
     end
 
     # return ""
-    return chatgpt(post_details.categories)
+    prompt = "write a short paragraph for a travel blog with keywords #{post_details.categories}"
+    if rand() * 10 >= 5
+      prompt += " in first person point of view."
+    end
+
+    puts "chatgpt prompt is : #{prompt}"
+    return chatgpt(prompt)
   end
 
   FLICKR_IMAGE_TEMPLATE = '
@@ -115,7 +170,7 @@ Not too shabby along the way too
 layout: post
 title: %{title}
 date: %{date}
-categories: %{categories}
+categories: [%{categories}]
 author: amit
 image: %{image_path}
 image_alt_text: %{image_alt_text}
@@ -127,12 +182,22 @@ photoset: %{photoset_id}
 %{flickr_images}
   '
 
+  def self.categorize(categories)
+    if categories.match?('travel')
+      'travel'
+    elsif categories.match?('hike|hiking|trail|mountain|climb')
+      'hiking'
+    else
+      'all'
+    end
+  end
+
   def self.create_post(post_details, other_photos_by_album, overwrite = true)
     post_hash = {
       post_file_name: post_details.post_file_name,
       title: post_details.photoset['title'],
       date: post_details.main_photo['datetaken'],
-      categories: post_details.categories,
+      categories: categorize(post_details.categories),
       image_path: post_details.main_photo['url_m'],
       image_alt_text: post_details.photoset['title'],
       featured: post_details.featured,
@@ -140,12 +205,25 @@ photoset: %{photoset_id}
       description: post_details.description
     }
 
+    required_fields = %w{ post_file_name title date image_path photoset_id }
+    required_fields.each do |required_field|
+      if post_hash[required_field] == '' || post_hash[required_field] == nil
+        puts "skipping entry for photo due to missing required field #{required_field}. Photo details : #{post_details.inspect}"
+        return
+      end
+    end
+
+    if post_details.post_id == ""
+      puts "cannot create post_id so skipping for post : #{post_details}"
+    end
     file_path = post_details.post_file_name
 
+    updating_post = false
     if File.exists? file_path
+      updating_post = true
       if overwrite
         puts 'overwrite flag is set so deleting existing file'
-        File.delete(file_path)
+        # File.delete(file_path)
       else
         puts file_path + ' already exists. NOT overriding'
         return
@@ -163,6 +241,7 @@ photoset: %{photoset_id}
       }
       str = FLICKR_IMAGE_TEMPLATE % hsh
       flickr_images += "{% #{str} %}\n"
+      # flickr_images += "{% flickr #{photo['id']} \"#{photo['title'] || ''}\" style=\"float: right;\" %}\n"
       break if i == 5
     end
 
@@ -179,57 +258,65 @@ photoset: %{photoset_id}
   def self.run
     data = get_flickr_updates()
 
-
     data[:post_details_by_id].each do |photoset_id, post_details|
+      raw_categories = post_details.main_photo.tags.split(' ').select {|tag| !['jsu', 'js', 'jsd', 'main'].include?(tag)}.uniq.join(' ')
       create_post(post_details, data[:other_photos_by_album_id])
     end
   end
 
-  def self.chatgpt(description)
+  def self.chatgpt_turbo_35(messages)
     OpenAI.configure do |config|
       config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
     end
 
     client = OpenAI::Client.new
 
-    prompt = "write description with keywords #{description}"
+    response = client.chat(
+      parameters: {
+        "model" => "gpt-3.5-turbo",
+        "messages" => messages,
+        "temperature" => 0,
+      }
+    )
 
-    # response = client.chat(
-    #   parameters: {
-    #     model: "text-davinci-003",
-    #     prompt: prompt,
-    #     # temperature: 0, # show the low risk text options
-    #     max_tokens: 256,
-    #     # frequency_penalty: 0,
-    #     # presence_penalty: 0,
-    # })
-    # puts response["choices"].map { |c| c["text"] }
+    puts '------------------'
+    puts messages.inspect
+    puts '------------------'
+    puts response.inspect
+    puts '------------------'
+    if response["choices"]
+      return response["choices"].first["message"]["content"]
+    else 
+      STDERR.puts response.inspect
+      return ""
+    end
+
+  end
+
+  def self.chatgpt(prompt)
+    OpenAI.configure do |config|
+      config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+    end
+
+    client = OpenAI::Client.new
 
     response = client.completions(
       parameters: {
           model: "text-davinci-003",
           prompt: prompt,
-          max_tokens: 256
+          max_tokens: 512
+    #     # temperature: 0, # show the low risk text options
+    #     # frequency_penalty: 0,
+    #     # presence_penalty: 0,
       })
 
     if response["choices"]
       puts response["choices"].map { |c| c["text"] }
 
       puts response.inspect
-      # => [", there lived a great"]
-
-        # response = client.chat(
-        #   parameters: {
-        #       model: "gpt-3.5-turbo", # Required.
-        #       messages: [{ role: "user", content: "Hello!"}], # Required.
-        #       temperature: 0.7,
-        #   })
-        # puts response.dig("choices", 0, "message", "content")
-
-        # => "Hello! How may I assist you today?"
-
       return response["choices"].first["text"] 
     else 
+      STDERR.puts response.inspect
       return ""
     end
 
